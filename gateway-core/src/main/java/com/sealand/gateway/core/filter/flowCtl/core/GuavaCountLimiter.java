35d19a2723b8f5e6d2be2350fecc6a233f614614
guohai <1299117865@qq.com>
2024-01-12 17:38:28 +0800

    guava 单机令牌桶限流更新

--- gateway-core/src/main/java/com/sealand/gateway/core/filter/flowCtl/core/GuavaCountLimiter.java
+++ gateway-core/src/main/java/com/sealand/gateway/core/filter/flowCtl/core/GuavaCountLimiter.java
@@ -1,7 +1,9 @@
 package com.sealand.gateway.core.filter.flowCtl.core;
 
 import com.sealand.common.config.Rule;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.curator.shaded.com.google.common.util.concurrent.RateLimiter;
+
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +14,14 @@ import java.util.concurrent.TimeUnit;
  * Guava RateLimiter提供了令牌桶算法实现：平滑突发限流(SmoothBurst)和平滑预热限流(SmoothWarmingUp)实现。
  */
 public class GuavaCountLimiter {
-    private RateLimiter rateLimiter;
-    private double maxPermits;
+    private final RateLimiter rateLimiter;
 
+    //最大令牌数
+    private final double maxPermits;
+
+    /**
+     * @param maxPermits 桶中最大的令牌数
+     */
     public GuavaCountLimiter(double maxPermits) {
         this.maxPermits = maxPermits;
         this.rateLimiter = RateLimiter.create(maxPermits);
@@ -30,8 +37,12 @@ public class GuavaCountLimiter {
 
 
     public static GuavaCountLimiter getInstance(String serviceId, Rule.FlowCtlConfig flowCtlConfig) {
-
-        //todo 判空
+        if (flowCtlConfig == null
+                || StringUtils.isEmpty(serviceId)
+                || StringUtils.isEmpty(flowCtlConfig.getConfig())
+                || StringUtils.isEmpty(flowCtlConfig.getValue())) {
+            return null;
+        }
         StringBuffer buffer = new StringBuffer();
         String key = buffer.append(serviceId).append(":").append(flowCtlConfig.getValue()).toString();
         GuavaCountLimiter guavaCountLimiter = resourceRateLimiterMap.get(key);
