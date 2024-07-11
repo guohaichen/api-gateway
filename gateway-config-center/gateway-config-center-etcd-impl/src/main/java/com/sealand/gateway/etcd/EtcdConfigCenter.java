577fd6d26e185eeaf25a8d177425bbb7743cc528
guohai <1299117865@qq.com>
2024-07-11 14:32:09 +0800

    etcd 配置中心&注册中心实现

--- gateway-config-center/gateway-config-center-etcd-impl/src/main/java/com/sealand/gateway/etcd/EtcdConfigCenter.java
+++ gateway-config-center/gateway-config-center-etcd-impl/src/main/java/com/sealand/gateway/etcd/EtcdConfigCenter.java
@@ -1,29 +1,147 @@
 package com.sealand.gateway.etcd;
 
+import com.alibaba.fastjson.JSON;
+import com.sealand.common.config.Rule;
+import com.sealand.common.constants.BasicConst;
 import com.sealand.gateway.config.center.api.ConfigCenter;
 import com.sealand.gateway.config.center.api.RulesChangeListener;
+import io.etcd.jetcd.ByteSequence;
 import io.etcd.jetcd.Client;
+import io.etcd.jetcd.KV;
+import io.etcd.jetcd.Watch;
+import io.etcd.jetcd.kv.GetResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author cgh
  * @create 2024/7/6
  * etcd 配置中心实现类
  */
+
+
+@Slf4j
 public class EtcdConfigCenter implements ConfigCenter {
 
 
     private Client client;
 
+    private static final String CONFIG = "/api-gateway/config/api-gateway";
+
+    private KV kvClient;
+
+    private String env;
+
+    private Watch watchClient;
 
     @Override
     public void init(String serverAddress, String env) {
-        client = Client.builder().endpoints(serverAddress).build();
+        this.client = Client.builder().endpoints(serverAddress).build();
+        this.kvClient = client.getKVClient();
+        this.watchClient = client.getWatchClient();
+        this.env = env;
+        ByteSequence configKey = ByteSequence.from((BasicConst.PATH_SEPARATOR + env + CONFIG).getBytes());
+
+        String value = "{\n" +
+                "  \"rules\": [\n" +
+                "    {\n" +
+                "      \"id\": \"001\",\n" +
+                "      \"name\": \"1号规则\",\n" +
+                "      \"protocol\": \"http\",\n" +
+                "      \"serviceId\": \"backend-http-server\",\n" +
+                "      \"prefix\": \"/http-server\",\n" +
+                "      \"paths\": [\n" +
+                "        \"/http-server/ping\",\n" +
+                "        \"/http-server/test\",\n" +
+                "        \"/http-server/post\"\n" +
+                "      ],\n" +
+                "      \"filterConfigs\": [\n" +
+                "        {\n" +
+                "          \"id\": \"load_balance_filter\",\n" +
+                "          \"config\": {\n" +
+                "            \"load_balance\": \"RoundRobin\"\n" +
+                "          }\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"id\": \"flow_ctl_filter\"\n" +
+                "        }\n" +
+                "      ],\n" +
+                "      \"flowCtlConfigs\": [\n" +
+                "        {\n" +
+                "          \"type\": \"path\",\n" +
+                "          \"model\": \"distributed_flowCtl\",\n" +
+                "          \"value\": \"/http-server/ping\",\n" +
+                "          \"config\": {\n" +
+                "            \"duration\": 20,\n" +
+                "            \"permits\": 2\n" +
+                "          }\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"id\": \"dubbo.100\",\n" +
+                "      \"name\": \"dubbo规则\",\n" +
+                "      \"protocol\": \"dubbo\",\n" +
+                "      \"serviceId\": \"backend-dubbo-server\",\n" +
+                "      \"prefix\": \"/dubbo\",\n" +
+                "      \"paths\": [\n" +
+                "        \"/dubbo-server/ping\",\n" +
+                "        \"/dubbo-server/list\",\n" +
+                "        \"/dubbo-server/user\"\n" +
+                "      ],\n" +
+                "      \"filterConfigs\": [\n" +
+                "        {\n" +
+                "          \"id\": \"dubbo_filter\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"id\": \"load_balance_filter\",\n" +
+                "          \"config\": {\n" +
+                "            \"load_balance\": \"RoundRobin\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+
+        kvClient.put(configKey, ByteSequence.from(value.getBytes()));
+        log.info("config load success...");
     }
 
 
     //todo
     @Override
     public void subscribeRulesChange(RulesChangeListener rulesChangeListener) {
+        log.info("etcd config init...");
+        ByteSequence configKey = ByteSequence.from((BasicConst.PATH_SEPARATOR + env + CONFIG).getBytes());
+        //todo bug 监听器事件
+        try {
 
+            kvClient =  Client.builder().endpoints("http://127.0.0.1:2379").build().getKVClient();
+            GetResponse getResponse = kvClient.get(configKey).get();
+            getResponse.getKvs().forEach(
+                    keyValue -> {
+                        List<Rule> rules = JSON.parseObject(keyValue.getValue().toString()).getJSONArray("rules").toJavaList(Rule.class);
+                        log.info("获取 etcd 配置，config: {}", keyValue.getValue().toString());
+                        rulesChangeListener.onRulesChange(rules);
+                        //监听器
+//                        watchClient.watch(configKey, Watch.listener(watchResponse -> {
+//                            watchResponse.getEvents().forEach(
+//                                    watchEvent -> {
+//                                        if (watchEvent.getEventType().equals(WatchEvent.EventType.PUT)) {
+//                                            KeyValue kv = watchEvent.getKeyValue();
+//                                            List<Rule> changeRules = JSON.parseObject(kv.getValue().toString()).getJSONArray("rules").toJavaList(Rule.class);
+//                                            rulesChangeListener.onRulesChange(changeRules);
+//                                        }
+//                                    }
+//                            );
+//                        }));
+                    }
+            );
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- gateway-register-center/gateway-register-center-etcd-impl/src/main/java/com/sealand/gateway/register/center/etcd/EtcdRegisterCenter.java
+++ gateway-register-center/gateway-register-center-etcd-impl/src/main/java/com/sealand/gateway/register/center/etcd/EtcdRegisterCenter.java
@@ -9,13 +9,14 @@ import com.sealand.gateway.register.center.api.RegisterCenterListener;
 import io.etcd.jetcd.ByteSequence;
 import io.etcd.jetcd.Client;
 import io.etcd.jetcd.KV;
-import io.etcd.jetcd.kv.GetResponse;
 import io.etcd.jetcd.options.GetOption;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.ExecutionException;
+import java.util.Set;
+import java.util.concurrent.*;
 
 /**
  * @author cgh
@@ -26,12 +27,20 @@ public class EtcdRegisterCenter implements RegisterCenter {
 
     private volatile Client client;
 
+    private String env;
+
     private KV kvClient;
 
     private final Object lock = new Object();
 
     private final String REGISTER_CENTER_PREFIX = "/api-gateway/service";
 
+    //服务定义前缀
+    private final String SERVICE_DEFINITION_PREFIX = "/api-gateway/service-definition";
+
+    /**
+     * 监听器列表
+     */
     private final List<RegisterCenterListener> registerCenterListenerList = new CopyOnWriteArrayList<>();
 
 
@@ -39,6 +48,7 @@ public class EtcdRegisterCenter implements RegisterCenter {
 
     @Override
     public void init(String registerAddress, String env) {
+        this.env = env;
         if (client == null) {
             synchronized (lock) {
                 if (client == null) {
@@ -51,40 +61,100 @@ public class EtcdRegisterCenter implements RegisterCenter {
 
     @Override
     public void register(ServiceDefinition serviceDefinition, ServiceInstance serviceInstance) {
-        ByteSequence key = ByteSequence.from((REGISTER_CENTER_PREFIX + separator + serviceDefinition.getServiceId() + separator
-                + serviceInstance.getIp() + BasicConst.COLON_SEPARATOR + serviceInstance.getPort()).getBytes());
-        log.info("etcd put key :{}", key);
-        ByteSequence value = ByteSequence.from(JSON.toJSONString(serviceInstance).getBytes());
+
+        //写入服务定义
+        String sd = separator + env + SERVICE_DEFINITION_PREFIX + separator + serviceDefinition.getServiceId();
+        ByteSequence serviceDefinitionKey = ByteSequence.from(sd.getBytes());
+        ByteSequence serviceDefinitionValue = ByteSequence.from(JSON.toJSONBytes(serviceDefinition));
         try {
-            kvClient.put(key, value).get();
+            //判断是否已经创建过服务实例
+            if (kvClient.get(serviceDefinitionKey).get().getKvs().isEmpty()) {
+                kvClient.put(serviceDefinitionKey, serviceDefinitionValue);
+            }
         } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException("etcd 创建服务示例失败：" + e);
+            throw new RuntimeException(e);
         }
+
+        //写入服务实例
+        ByteSequence serviceInstanceKey = ByteSequence.from((separator + env + REGISTER_CENTER_PREFIX + separator + serviceDefinition.getServiceId() + separator
+                + serviceInstance.getIp() + BasicConst.COLON_SEPARATOR + serviceInstance.getPort()).getBytes());
+        log.info("etcd put serviceInstanceKey: {}", serviceInstanceKey);
+        ByteSequence serviceInstanceValue = ByteSequence.from(JSON.toJSONBytes(serviceInstance));
+        kvClient.put(serviceInstanceKey, serviceInstanceValue);
+
     }
 
     @Override
     public void deregister(ServiceDefinition serviceDefinition, ServiceInstance serviceInstance) {
-        ByteSequence key = ByteSequence.from((REGISTER_CENTER_PREFIX + separator + serviceDefinition.getServiceId() + separator
+        ByteSequence key = ByteSequence.from((separator + env + REGISTER_CENTER_PREFIX + separator + serviceDefinition.getServiceId() + separator
                 + serviceInstance.getIp() + BasicConst.COLON_SEPARATOR + serviceInstance.getPort()).getBytes());
         kvClient.delete(key);
+
+        //serviceDefinitionList.remove(separator + env + REGISTER_CENTER_PREFIX + separator + serviceDefinition.getServiceId());
         log.info("{} 服务实例下线...", serviceDefinition.getServiceId() + separator + serviceInstance.getIp() + separator + serviceInstance.getPort());
     }
 
+    //todo 缺少服务实例/定义变化的回调，更新DynamicConfigManager
     @Override
     public void subscribeAllServicesChange(RegisterCenterListener registerCenterListener) {
-        /* 根据服务前缀key，找到所有的服务定义，找到所有的服务实例；
-        监听所有的服务实例，如果服务实例发生变化，更新
-         */
+        registerCenterListenerList.add(registerCenterListener);
+
+        doSubscribeChange();
+        ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(1);
+
+        scheduledThreadPool.scheduleWithFixedDelay(this::doSubscribeChange, 10, 10, TimeUnit.SECONDS);
+    }
+
+    private void doSubscribeChange() {
+        List<ServiceDefinition> serviceDefinitionList = new ArrayList<>();
+
         //服务定义前缀
-        ByteSequence prefixKey = ByteSequence.from((REGISTER_CENTER_PREFIX + separator).getBytes());
-        //使用前缀查询
-        GetOption getOption = GetOption.newBuilder().isPrefix(true).build();
+        ByteSequence prefixKey = ByteSequence.from((separator + env + SERVICE_DEFINITION_PREFIX + separator).getBytes());
+        GetOption prefixOption = GetOption.newBuilder().isPrefix(true).build();
         try {
-            GetResponse response = kvClient.get(prefixKey, getOption).get();
+            kvClient.get(prefixKey, prefixOption).get().getKvs().forEach(keyValue -> {
+                serviceDefinitionList.add(JSON.parseObject(keyValue.getValue().toString(), ServiceDefinition.class));
+            });
         } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
         }
 
 
+        //找到所有的服务定义
+        for (ServiceDefinition sd : serviceDefinitionList) {
+
+            String key = separator + env + REGISTER_CENTER_PREFIX + separator + sd.getServiceId();
+            log.info("key :{} ", key);
+            ByteSequence watchKey = ByteSequence.from(key.getBytes());
+
+            //todo 应该写监听器事件实现，目前硬编码
+            Set<ServiceInstance> serviceInstanceSet = new HashSet<>();
+            try {
+                kvClient.get(watchKey, GetOption.newBuilder().isPrefix(true).build()).get().getKvs().forEach(
+                        keyValue -> {
+                            serviceInstanceSet.add(JSON.parseObject(keyValue.getValue().toString(), ServiceInstance.class));
+                        }
+                );
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+            log.info("register listener onchange 回调...");
+            registerCenterListenerList.forEach(l -> l.onChange(sd, serviceInstanceSet));
+
+//            //实例化一个监听器对象，当指定key，注意 watch机制包含前缀监听  发生变化时被调用
+//            client.getWatchClient().watch(watchKey, Watch.listener(watchResponse -> {
+//                Set<ServiceInstance> instanceSet = new HashSet<>();
+//                watchResponse.getEvents().forEach(
+//                        watchEvent -> {
+//                            if (WatchEvent.EventType.PUT.equals(watchEvent.getEventType())) {
+//                                log.info("--------------watch listener----------");
+//                                instanceSet.add(JSON.parseObject(watchEvent.getKeyValue().getValue().toString(), ServiceInstance.class));
+//                            }
+//                        }
+//                );
+//                log.info("onchange回调...");
+//                registerCenterListenerList.forEach(l -> l.onChange(sd, instanceSet));
+//            }));
+        }
     }
 }
